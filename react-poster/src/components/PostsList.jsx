e80d7b20ccf041bd529d72d39dc20de860466502
kastastin <kostya242421@gmail.com>
2024-02-21 13:13:28 +0200

    reset

---
+++ react-poster/src/components/PostsList.jsx
@@ -0,0 +1,32 @@
+import { useLoaderData } from "react-router-dom";
+
+import Post from "./Post";
+import classes from "./PostsList.module.css";
+
+const PostsList = () => {
+	const posts = useLoaderData();
+
+	return (
+		<>
+			{posts.length > 0 ? (
+				<ul className={classes.posts}>
+					{posts.map((post) => (
+						<Post
+							key={post.id}
+							id={post.id}
+							author={post.author}
+							body={post.body}
+						/>
+					))}
+				</ul>
+			) : (
+				<div style={{ textAlign: "center", color: "white" }}>
+					<h2>There are no posts yet.</h2>
+					<p>Start adding some!</p>
+				</div>
+			)}
+		</>
+	);
+};
+
+export default PostsList;
---
+++ react-poster/src/routes/Posts.jsx
@@ -0,0 +1,23 @@
+import { Outlet } from "react-router-dom";
+
+import PostsList from "../components/PostsList";
+
+function Posts() {
+	return (
+		<>
+			<Outlet />
+			<main>
+				<PostsList />
+			</main>
+		</>
+	);
+}
+
+export default Posts;
+
+export async function loader() {
+	const res = await fetch("http://localhost:8080/posts");
+	const resData = await res.json();
+
+	return resData.posts;
+}
