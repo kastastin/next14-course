33a5305c3623ba4e19fe4e5c0cc04e217508c7b6
kastastin <kostya242421@gmail.com>
2024-02-21 18:16:49 +0200

    better-sqlite3

--- foodies/app/meals/page.js
+++ foodies/app/meals/page.js
@@ -1,3 +1,29 @@
+import Link from "next/link";
+
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+
 export default function MealPage() {
-	return <h1>Meals page</h1>;
+	return (
+		<>
+			<header className={classes.header}>
+				<h1>
+					Delicious meals, created{" "}
+					<span className={classes.hightlight}>by you</span>
+				</h1>
+
+				<p>
+					Choose your favorite recipe and cook it yourself! It is easy and fun!
+				</p>
+
+				<p className={classes.cta}>
+					<Link href="/meals/share">Share Your Favorite Recipe</Link>
+				</p>
+			</header>
+
+			<main className={classes.main}>
+				<MealsGrid meals={[]} />
+			</main>
+		</>
+	);
 }
