34989095f522eb5412d492eec6f8e1f940051a4d
kastastin <kostya242421@gmail.com>
2024-02-25 00:51:03 +0200

    storing data in db, upload image, useFormStatus

--- foodies/app/meals/share/page.js
+++ foodies/app/meals/share/page.js
@@ -1,21 +1,10 @@
+import { shareMeal } from "@/lib/actions";
 import ImagePicker from "@/components/meals/image-picker";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
-	async function shareMeal(formData) {
-		"use server";
-
-		const meal = {
-			title: formData.get("title"),
-			summary: formData.get("summary"),
-			instructions: formData.get("instructions"),
-			image: formData.get("image"),
-			creator: formData.get("name"),
-			creator_email: formData.get("email"),
-		};
-	}
-
 	return (
 		<>
 			<header className={classes.header}>
@@ -56,7 +45,7 @@ export default function ShareMealPage() {
 					</p>
 					<ImagePicker label="Your image" name="image" />
 					<p className={classes.actions}>
-						<button type="submit">Share Meal</button>
+						<MealsFormSubmit />
 					</p>
 				</form>
 			</main>
