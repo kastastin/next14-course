6a25b4a3053f8aab47d443aed26ca7512fbbacba
kastastin <kostya242421@gmail.com>
2024-02-28 22:18:04 +0200

    context state

--- next-events/store/notification-context.js
+++ next-events/store/notification-context.js
@@ -1,14 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const NotificationContext = createContext({
 	notification: null,
-	showNotification: function () {},
+	showNotification: function (notificationData) {},
 	hideNotification: function () {},
 });
 
 export function NotificationContextProvider({ children }) {
+	const [activeNotification, setActiveNotification] = useState(null);
+
+	const context = {
+		notification: activeNotification,
+		showNotification: showNotificationHandler,
+		hideNotification: hideNotificationHandler,
+	};
+
+	function showNotificationHandler(notificationData) {
+		setActiveNotification(notificationData);
+	}
+
+	function hideNotificationHandler() {
+		setActiveNotification(null);
+	}
+
 	return (
-		<NotificationContext.Provider>{children}</NotificationContext.Provider>
+		<NotificationContext.Provider value={context}>
+			{children}
+		</NotificationContext.Provider>
 	);
 }
 
