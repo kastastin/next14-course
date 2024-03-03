eb4da9365937864f98a4ee1a59b242e526ba79b7
kastastin <kostya242421@gmail.com>
2024-03-04 00:11:20 +0200

    Adding Functions To Read & Fetch Data From Markdown Files

---
+++ next-blog/lib/posts-util.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export function getAllPosts() {
+	const postFiles = fs.readdirSync(postsDirectory);
+
+	const allPosts = postFiles.map((postFile) => {
+		return getPostData(postFile);
+	});
+
+	const sortedPosts = allPosts.sort((postA, postB) =>
+		postA.date > postB.date ? -1 : 1
+	);
+
+	return sortedPosts;
+}
+
+export function getFeaturedPosts() {
+	const allPosts = getAllPosts();
+	const featuredPosts = allPosts.filter((post) => post.isFeatured);
+
+	return featuredPosts;
+}
+
+function getPostData(postIdentifier) {
+	const filePath = path.join(postsDirectory, `${postSlug}.md`);
+	const fileContent = fs.readFileSync(filePath, "utf-8");
+	const { data, content } = matter(fileContent);
+
+	// Removes the file extension
+	const postSlug = postIdentifier.replace(/\.md$/, "");
+
+	const postData = {
+		slug: postSlug,
+		...data,
+		content,
+	};
+
+	return postData;
+}
+
+// Adding Functions To Read & Fetch Data From Markdown Files
