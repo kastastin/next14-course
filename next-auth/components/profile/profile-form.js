cc23bf07180308a4fd276bb87f3410ec8459f3b8
kastastin <kostya242421@gmail.com>
2024-03-12 00:04:06 +0200

    Sending a "Change Password" Request From The Frontend

--- next-auth/components/profile/profile-form.js
+++ next-auth/components/profile/profile-form.js
@@ -1,16 +1,33 @@
+import { useRef } from "react";
+
 import classes from "./profile-form.module.css";
 
-export default function ProfileForm() {
+export default function ProfileForm({ onChangePassword }) {
+	const newPasswordInputRef = useRef();
+	const oldPasswordInputRef = useRef();
+
+	function submitHandler(e) {
+		e.preventDefault();
+
+		const enteredNewPassword = newPasswordInputRef.current.value;
+		const enteredOldPassword = oldPasswordInputRef.current.value;
+
+		onChangePassword({
+			newPassword: enteredNewPassword,
+			oldPassword: enteredOldPassword,
+		});
+	}
+
 	return (
-		<form className={classes.form}>
+		<form className={classes.form} onSubmit={submitHandler}>
 			<div className={classes.control}>
 				<label htmlFor="new-password">New Password</label>
-				<input type="password" id="new-password" />
+				<input id="new-password" type="password" ref={newPasswordInputRef} />
 			</div>
 
 			<div className={classes.control}>
 				<label htmlFor="old-password">Old Password</label>
-				<input type="password" id="old-password" />
+				<input id="old-password" type="password" ref={oldPasswordInputRef} />
 			</div>
 
 			<div className={classes.action}>
