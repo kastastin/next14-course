0cf6752d59ea140f9d7b1170a88e515913f67871
kastastin <kostya242421@gmail.com>
2024-03-12 03:18:19 +0200

    dynamic path params in getStaticProps + getStaticPaths

--- next-meetups/pages/[meetupId].js
+++ next-meetups/pages/[meetupId].js
@@ -10,3 +10,38 @@ export default function MeetupDetails() {
 		/>
 	);
 }
+
+export async function getStaticProps(context) {
+	const { meetupId } = context.params;
+
+	return {
+		props: {
+			meetupData: {
+				id: meetupId,
+				image:
+					"https://images.unsplash.com/photo-1546195643-70f48f9c5b87?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGxvY2FsJTIwcGxhY2V8ZW58MHx8MHx8fDA%3D",
+				title: "First meetup",
+				address: "Random address",
+				description: "Description",
+			},
+		},
+	};
+}
+
+export async function getStaticPaths() {
+	return {
+		fallback: false,
+		paths: [
+			{
+				params: {
+					meetupId: "m1",
+				},
+			},
+			{
+				params: {
+					meetupId: "m2",
+				},
+			},
+		],
+	};
+}
