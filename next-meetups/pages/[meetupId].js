077ba6945f8593eaeec6aa194ffbece35fe5a4ee
kastastin <kostya242421@gmail.com>
2024-03-12 02:36:31 +0200

    Detail page, custom component + styles

--- next-meetups/pages/[meetupId].js
+++ next-meetups/pages/[meetupId].js
@@ -1,7 +1,12 @@
-export default function MeetupDetail() {
+import MeetupDetail from "../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
 	return (
-		<div>
-			<h1>Meetup Detail Page</h1>
-		</div>
+		<MeetupDetail
+			image="https://images.unsplash.com/photo-1546195643-70f48f9c5b87?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGxvY2FsJTIwcGxhY2V8ZW58MHx8MHx8fDA%3D"
+			title="First meetup"
+			address="Random address"
+			description="Description"
+		/>
 	);
 }
